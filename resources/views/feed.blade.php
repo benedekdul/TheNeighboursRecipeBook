e135538948e03045c45cf093acb61af58938badc
Benedek Dul <c5fqe7@inf.elte.hu>
2022-10-06 17:16:35 +0200

    created 'feed' page & got rid of default welcome page

---
+++ resources/views/feed.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('title')
+    Feed
+@endsection
+
+@section('content')
+<h1>Feed</h1>
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed condimentum justo non lacus semper, ut mattis nunc tempor. Donec consequat ligula massa, ac efficitur diam convallis eu. Nullam ut metus pharetra, congue leo et, semper ex. Curabitur condimentum mi in placerat accumsan. Integer placerat est vitae viverra gravida. Vestibulum augue nisl, pretium non consequat at, ornare nec odio. Fusce convallis, ante ut commodo laoreet, arcu tortor vulputate arcu, eu porttitor metus metus non risus. Proin et velit elit. Vivamus sit amet ex in mauris eleifend porttitor vel ac ex. 
+
+
+@endsection
