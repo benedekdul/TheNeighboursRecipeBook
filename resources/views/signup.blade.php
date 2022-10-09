ca17201ed7bb719164ced7a5128eb510b51189b3
Benedek Dul <c5fqe7@inf.elte.hu>
2022-10-09 15:09:11 +0200

    redesigned register page

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -5,27 +5,38 @@
 @endsection
 
 @section('content')
-@include('includes.display_errors')
-<div class="row">
-    <div class="col-md-6">
-        <h3>register</h3>
-        <form action="{{ route('signup') }}" method="post">
-            <div class="form-group">
-                <label for="username">Username</label>
-                <input class="form-control {{ $errors->has('username') ? 'is-invalid' : '' }}" type="text" name="username" id="username" value="{{ Request::old('username') }}">
+
+<div class="half">
+    <div class="bg order-1 order-md-2" style="background-color:ghostwhite"></div>
+    <div class="contents order-2 order-md-1">
+      <div class="container">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-md-6">
+            <div class="form-block">
+              <div class="text-center mb-5">
+              <h3>Join the <strong>TNRB</strong> experience now!</h3>
+              <!-- <p class="mb-4">Lorem ipsum dolor sit amet elit. Sapiente sit aut eos consectetur adipisicing.</p> -->
+              </div>
+              <form action="{{ route('signup') }}" method="post">
+                <input type="hidden" name="_token" value="{{ Session::token() }}">
+                <div class="form-group first">
+                  {{-- <label for="username">Username or email</label> --}}
+                  <input type="text" class="form-control {{ $errors->has('username') ? 'is-invalid' : '' }}" placeholder="Username..." name="username" value="{{ Request::old('username') }}">
+                </div>
+                <div class="form-group first">
+                    <input type="text" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" placeholder="E-mail address..." name="email" value="{{ Request::old('email') }}">
+                </div>
+                <div class="form-group last mb-3">
+                  {{-- <label for="password">Password</label> --}}
+                  <input type="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" placeholder="Your Password (must be at least 4 characters long)" name="password" value="{{ Request::old('password') }}">
+                </div>
+                <input type="submit" value="Create my kitchen" class="btn btn-block btn-warning">
+              </form>
             </div>
-            <div class="form-group">
-                <label for="email">E-mail</label>
-                <input class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" type="text" name="email" id="email" value="{{ Request::old('email') }}">
-            </div>
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input class="form-control {{ $errors->has('password') ? 'is-invalid' : 'x' }}" type="password" name="password" id="password" value="{{ Request::old('password') }}">
-            </div>
-            <button type="submit" class="btn btn-primary">register</button>
-            <input type="hidden" name="_token" value="{{ Session::token() }}">
-        </form>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
 
 @endsection
