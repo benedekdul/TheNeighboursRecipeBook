6eb9c812e0c74a35cbc121c601caa5d70a3e9d56
Rozafa Zabergja <rozafazabergja@Rozafas-MacBook-Air.local>
2022-10-16 16:00:06 +0200

    added Post Model & Migration

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -5,9 +5,15 @@
 @endsection
 
 @section('content')
-<h1>Feed</h1>
+    <div class = "container">
+        <div class = "row">
+            <h1>Feed</h1>
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed condimentum justo non lacus semper, ut mattis nunc tempor. Donec consequat ligula massa, ac efficitur diam convallis eu. Nullam ut metus pharetra, congue leo et, semper ex. Curabitur condimentum mi in placerat accumsan. Integer placerat est vitae viverra gravida. Vestibulum augue nisl, pretium non consequat at, ornare nec odio. Fusce convallis, ante ut commodo laoreet, arcu tortor vulputate arcu, eu porttitor metus metus non risus. Proin et velit elit. Vivamus sit amet ex in mauris eleifend porttitor vel ac ex. 
-
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed condimentum justo non lacus semper, ut mattis nunc tempor. Donec consequat ligula massa, ac efficitur diam convallis eu. Nullam ut metus pharetra, congue leo et, semper ex. Curabitur condimentum mi in placerat accumsan. Integer placerat est vitae viverra gravida. Vestibulum augue nisl, pretium non consequat at, ornare nec odio. Fusce convallis, ante ut commodo laoreet, arcu tortor vulputate arcu, eu porttitor metus metus non risus. Proin et velit elit. Vivamus sit amet ex in mauris eleifend porttitor vel ac ex. 
+        </div>  
+        <div class = "row">   
+            <a href="#"> Add New Post </a>
+        </div>    
+</div>
 
 @endsection
