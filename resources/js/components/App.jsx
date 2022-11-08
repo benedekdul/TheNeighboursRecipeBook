23d982dc0986c54fe8774627498139f8ce8b1389
Benedek Dul <c5fqe7@inf.elte.hu>
2022-11-08 23:59:58 +0100

    setup react

---
+++ resources/js/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                {/* components go here */}
+            </div>
+        </div>
+    );
+}
+
+export default App;
