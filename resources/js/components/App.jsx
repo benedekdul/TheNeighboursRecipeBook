1ad27db7f745b1a30d9f5933162ca4caa0b96ca0
Benedek Dul <c5fqe7@inf.elte.hu>
2022-11-09 13:12:05 +0100

    New component: navbar (Header)

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../../css/style.css'
 import ListView from './feed/ListView';
+import Header from './Header';
+
 
 function App() {
     return (
         <div className="container">
             <div className="row justify-content-center">
                 {/* components go here */}
-                
+                <Header/>
+
                 <ListView/>
             </div>
         </div>
