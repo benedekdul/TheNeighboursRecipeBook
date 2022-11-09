6e2e1460280949713fa53758ae96b8d1769a39ab
TheHagen98 <61209594+TheHagen98@users.noreply.github.com>
2022-11-09 21:00:21 +0100

    Create reacttest.js
    Create example test file

---
+++ __tests__/reacttest.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+it("renders with or without a name", () => {
+  act(() => {
+   
+  });
+  expect(true).toBe(true);
+});
