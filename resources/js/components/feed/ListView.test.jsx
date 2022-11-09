8e35f757b49e3a1f181db8bbf356210fe9c37d35
Hagen Botond Benedek <iptkih@inf.elte.hu>
2022-11-09 21:49:00 +0100

    Created empty ListView test

---
+++ resources/js/components/feed/ListView.test.jsx
@@ -0,0 +1,9 @@
+import { render } from "@testing-libary/react";
+import ListView from "./ListView";
+
+describe("ListView Component",() => {
+
+    it ("rendered listview", () => {
+        expect(true).toBeTruthy();
+    });
+});
