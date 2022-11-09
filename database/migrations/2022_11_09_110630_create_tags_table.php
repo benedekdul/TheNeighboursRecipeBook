b49034549a69b7007b1b395b0b5d3064f4914470
Benedek Dul <c5fqe7@inf.elte.hu>
2022-11-09 12:08:41 +0100

    added post id, name columns to migration

--- database/migrations/2022_11_09_110630_create_tags_table.php
+++ database/migrations/2022_11_09_110630_create_tags_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->integer('post_id');
+            $table->string('name');
+            // $table->timestamps();
         });
     }
 
