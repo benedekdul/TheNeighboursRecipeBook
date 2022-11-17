520b8da34993c8999c4e65276b6c9d228bc5522c
Benedek Dul <c5fqe7@inf.elte.hu>
2022-11-18 00:20:53 +0100

    created fullpost component

---
+++ resources/js/components/feed/FullPost.jsx
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class FullPost extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default ListItemLeft;
