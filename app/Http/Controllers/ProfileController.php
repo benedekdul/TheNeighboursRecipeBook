24bd21e3ec4372cf45cd5ae45c8f1b5591df3f69
Rozafa Zabergja <rozafazabergja@Rozafas-MacBook-Air.local>
2022-11-18 02:26:13 +0100

    Solved issues on getProfileFromUser function in ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Profile;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
     public function getProfileFromUser($user_id){
 
-        $profile = Profile::find($user_id);
-        //$posts = Post::where($user_id, 'user_id'); //not working
+        $profile = User::find($user_id)->profile()->get();
+
         return response()->json([
             'status' => 200, 
             'profiles' => $profile
